e14425562391f96fb12bcbd91c72af4f15553445
n <n@n.n>
2025-07-29 19:38:26 +0200

    first commit

---
+++ src/main/java/es/cic/curso25/proyecto009/Proyecto009Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyecto009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto009Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto009Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/proyecto009/Proyecto009ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proyecto009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto009ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
